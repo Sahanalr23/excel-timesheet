e1b9b6fbbd50f4fbd0a4ef3859c76b864edb8fcb
unknown <sahanasahana331@gmail.com>
2024-02-21 00:00:46 +0530

    Add all my files

--- assets/js/main.js
+++ assets/js/main.js
@@ -99,11 +99,11 @@
    * Clients Slider
    */
   new Swiper('.clients-slider', {
-    speed: 400,
+    speed: 7000,
     loop: true,
     autoplay: {
-      delay: 2000,
-      disableOnInteraction: false
+      delay: 100,
+      disableOnInteraction: false,
     },
     slidesPerView: 'auto',
     pagination: {
@@ -130,6 +130,7 @@
       }
     }
   });
+
 
   /**
    * Portfolio isotope and filter
@@ -186,34 +187,7 @@
     }
   });
 
-  /**
-   * Testimonials slider
-   */
-  new Swiper('.testimonials-slider', {
-    speed: 600,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    slidesPerView: 'auto',
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-    },
-    breakpoints: {
-      320: {
-        slidesPerView: 1,
-        spaceBetween: 20
-      },
 
-      1200: {
-        slidesPerView: 3,
-        spaceBetween: 20
-      }
-    }
-  });
 
   /**
    * Animation on scroll

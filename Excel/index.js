629b0a0bcb8d78da16492a55253ac04fc76a915a
Sahana L R <sahanasahana331@gmail.com>
2024-12-06 20:10:57 +0530

    Add files via upload

--- Excel/index.js
+++ Excel/index.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors({ origin: 'https://jaykishanp.github.io', methods: ['GET', 'POST'], allowedHeaders: ['Content-Type'] }));
+app.use(cors({ origin: 'https://sahanalr23.github.io', methods: ['GET', 'POST'], allowedHeaders: ['Content-Type'] }));
 app.use(express.json());
 
 // Verify GOOGLE_CREDENTIALS_BASE64 exists
